1de1ec30b28aba530bb3adcc5dd32c52e49abec4
Amodu <amodudavid777@gmail.com>
2023-09-09 00:03:54 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+import pytz
+import os
+
+time = datetime.now(pytz.UTC).strftime('%Y-%d-%HT%H:%M:%SZ')
+month = datetime.now(pytz.UTC).strftime('%A')
+print(time, month)
+
+app = Flask(__name__)
+app.config['SECRET_key'] = os.environ.get('s_key')
+
+@app.route('/')
+def home():
+
+    return jsonify(slack_name=request.args.get('slack_name'),current_day=month, utc_time=time, track=request.args.get('track'),
+                   github_file_url='https://github.com/Am0du/HNG-task/Taskone/main.py', github_repo_url='https://github.com/username/repo',
+                   status_code=200), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
