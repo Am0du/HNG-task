bbe1a479fe9b4d53e851e6df91996b28610f9688
Amodu <amodudavid777@gmail.com>
2023-09-09 04:00:02 +0100

    fix the time format

--- Taskone/main.py
+++ Taskone/main.py
@@ -3,14 +3,14 @@ from datetime import datetime, timedelta
 import pytz
 import os
 
-
-
 app = Flask(__name__)
 app.config['SECRET_key'] = os.environ.get('s_key')
+
 
 @app.route('/')
 def home():
     return jsonify(response='Hello')
+
 
 @app.route('/api')
 def api():
@@ -22,32 +22,15 @@ def api():
 
     plus_time = plus_time.strftime('%Y-%m-%dT%H:%M:%SZ')
     minus_time = minus_time.strftime('%Y-%m-%dT%H:%M:%SZ')
-    time = datetime.now(pytz.UTC).strftime('%Y-%m-%dT%-H:%M:%SZ')
+    time = datetime.now(pytz.UTC).strftime('%Y-%m-%dT%#H:%M:%SZ')
 
-    # if time == minus_time:
-        return jsonify(slack_name=request.args.get('slack_name'),
-                       current_day=month,
-                       utc_time=time,
-                       track=request.args.get('track'),
-                       github_file_url='https://github.com/Am0du/HNG-task/Taskone/main.py',
-                       github_repo_url='https://github.com/username/repo',
-                       status_code=200), 200
-    # elif time == plus_time:
-    #     return jsonify(slack_name=request.args.get('slack_name'),
-    #                    current_day=month,
-    #                    utc_time=plus_time,
-    #                    track=request.args.get('track'),
-    #                    github_file_url='https://github.com/Am0du/HNG-task/Taskone/main.py',
-    #                    github_repo_url='https://github.com/username/repo',
-    #                    status_code=200), 200
-    # else:
-    #     return jsonify(slack_name=request.args.get('slack_name'),
-    #                    current_day=month,
-    #                    utc_time=time,
-    #                    track=request.args.get('track'),
-    #                    github_file_url='https://github.com/Am0du/HNG-task/Taskone/main.py',
-    #                    github_repo_url='https://github.com/username/repo',
-    #                    status_code=200), 200
+    return jsonify(slack_name=request.args.get('slack_name'),
+                   current_day=month,
+                   utc_time=time,
+                   track=request.args.get('track'),
+                   github_file_url='https://github.com/Am0du/HNG-task/Taskone/main.py',
+                   github_repo_url='https://github.com/username/repo',
+                   status_code=200), 200
 
 
 if __name__ == '__main__':
