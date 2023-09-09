339965171d300f3fbadb05ef994bcc76bd1d8b5f
Amodu <amodudavid777@gmail.com>
2023-09-09 01:56:58 +0100

    added a new route

--- Taskone/main.py
+++ Taskone/main.py
@@ -11,7 +11,10 @@ app.config['SECRET_key'] = os.environ.get('s_key')
 
 @app.route('/')
 def home():
+    return jsonify(response='Hello')
 
+@app.route('/api')
+def api():
     return jsonify(slack_name=request.args.get('slack_name'),current_day=month, utc_time=time, track=request.args.get('track'),
                    github_file_url='https://github.com/Am0du/HNG-task/Taskone/main.py', github_repo_url='https://github.com/username/repo',
                    status_code=200), 200
